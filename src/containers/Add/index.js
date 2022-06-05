7e0494f2f981f7cecee297cb01d9a9f7941b9b99
Pim Derks <info@pimderks.com>
2022-06-05 16:37:52 +0200

    Navigate back to overview after submit.

--- src/containers/Add/index.js
+++ src/containers/Add/index.js
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import PasswordForm from "../../components/PasswordForm";
 import UserContext, { addPassword } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
   const { passwords, setPasswords } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const onSubmit = (pw) => {
     setPasswords(addPassword(passwords, pw));
+    navigate("/");
   };
 
   return <PasswordForm onSubmit={onSubmit} />;
